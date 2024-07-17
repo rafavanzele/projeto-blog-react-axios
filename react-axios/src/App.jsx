54568dd1485cd9b90e9d3803d7069d3989de9487
rafavanzele <rafa_vanzele@hotmail.com>
2024-07-17 17:08:07 -0300

    navbar / css / resgatando dados da api

--- react-axios/src/App.jsx
+++ react-axios/src/App.jsx
@@ -1,12 +1,18 @@
 import { Outlet } from 'react-router-dom'
 
+// ESTILOS
 import './App.css'
+
+// COMPONENTES
+import Navbar from './components/Navbar'
 
 function App() {
   
 
   return (
     <div className="App">
+      <Navbar/>
+
       <div className="container">
         <Outlet/>
       </div>
