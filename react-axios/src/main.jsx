70877123d7d1fa38e4789e75b179668b622ccd03
rafavanzele <rafa_vanzele@hotmail.com>
2024-07-17 11:38:06 -0300

    projeto blog com react e axios - setup iniicial

---
+++ react-axios/src/main.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
+
+// PAGES
+import Home from './routes/Home.jsx'
+import NewPost from './routes/NewPost.jsx'
+
+// ESTILOS
+import './index.css'
+
+// CONFIG DE PÁGINAS
+const router = createBrowserRouter([
+  {
+    element: <App/>,
+    children: [
+      {
+        path: '/',
+        element: <Home/>
+      },
+      {
+        path: '/new',
+        element: <NewPost/>
+      },
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
