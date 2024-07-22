08f02c59ab854cd41114531926e440de308b46a1
rafavanzele <rafa_vanzele@hotmail.com>
2024-07-22 17:48:04 -0300

    editando post

--- react-axios/src/routes/EditPost.jsx
+++ react-axios/src/routes/EditPost.jsx
@@ -13,6 +13,38 @@ const EditPost = () => {
     const {id} = useParams()
 
 
+    const getPost = async () => {
+        try {
+            const response = await blogFetch.get(`/posts/${id}`)
+
+            const data = response.data
+
+            setTitle(data.title)
+            setBody(data.body)
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+    const editPost = async (e) => {
+        e.preventDefault()
+
+        const post = {title, body, userId: 1}
+
+        await blogFetch.put(`/posts/${id}`, {
+            body: post,
+        })
+    }
+
+
+    useEffect(() => {
+        getPost()
+    }, [])
+
+
+
   return (
     <div className='new-post'>
       <h2>Editando: {title}</h2>
@@ -20,12 +52,12 @@ const EditPost = () => {
       <form onSubmit={(e) => editPost(e)}>
         <div className="form-control">
           <label htmlFor="title">Título: </label>
-          <input type="text" name='title' id='title' placeholder='Digite o título' onChange={(e) => setTitle(e.target.value)}/>
+          <input type="text" name='title' id='title' placeholder='Digite o título' onChange={(e) => setTitle(e.target.value)} value={title || ''}/>
         </div>
 
         <div className="form-control">
           <label htmlFor="body">Conteúdo: </label>
-          <textarea name="body" id="body" placeholder='Digite o conteúdo' onChange={(e) => setBody(e.target.value)}></textarea>
+          <textarea name="body" id="body" placeholder='Digite o conteúdo' onChange={(e) => setBody(e.target.value)} value={body || ''}></textarea>
         </div>
 
         <input type="submit" value={'Editar post'} className="btn"/>
